fe760adaf267bede6eed4c494673c74a6c46d051
René Buffat <buffat@gmail.com>
2018-04-02 22:33:29 +0200

    simplify config classes

--- pytempsense/bme280api.pyx
+++ pytempsense/bme280api.pyx
@@ -39,94 +39,37 @@ cdef inline check_error(int8_t error_code, txt):
 
 
 class BME280SensorPowerMode:
-    SLEEP = "SLEEP_MODE"
-    FORCED = "FORCED"
-    NORMAL = "NORMAL"
+    SLEEP = 0
+    FORCED = 1
+    NORMAL = 3
 
 
 class BME280Oversampling:
-    NO = "NO"
-    X1 = "X1"
-    X2 = "X2"
-    X4 = "X4"
-    X8 = "X8"
-    X16 = "X16"
+    NO = 0
+    X1 = 1
+    X2 = 2
+    X4 = 3
+    X8 = 4
+    X16 = 5
 
 
 class BME280StandbyTime:
-    TIME_1_MS = "1 MS"
-    TIME_10_MS = "10 MS"
-    TIME_20_MS = "20 MS"
-    TIME_62_5_MS = "62.5 MS"
-    TIME_125_MS = "125 MS"
-    TIME_250_MS = "250 MS"
-    TIME_500_MS = "500 MS"
-    TIME_1000_MS = "1000 MS"
+    TIME_1_MS = 0
+    TIME_10_MS = 6
+    TIME_20_MS = 7
+    TIME_62_5_MS = 1
+    TIME_125_MS = 2
+    TIME_250_MS = 3
+    TIME_500_MS = 4
+    TIME_1000_MS = 5
 
 
 class BME280Filter:
-    OFF = "OFF"
-    COEFF_2 = "COEFF_2"
-    COEFF_4 = "COEFF_4"
-    COEFF_8 = "COEFF_8"
-    COEFF_16 = "COEFF_16"
-
-
-cdef inline _map_mode(mode):
-    if mode == BME280SensorPowerMode.SLEEP:
-        return BME280_SLEEP_MODE
-    if mode == BME280SensorPowerMode.FORCED:
-        return BME280_FORCED_MODE
-    if mode == BME280SensorPowerMode.NORMAL:
-        return BME280_NORMAL_MODE
-    return None
-
-cdef inline _map_oversampling(oversampling):
-    if oversampling == BME280Oversampling.NO:
-        return BME280_NO_OVERSAMPLING
-    if oversampling == BME280Oversampling.X1:
-        return BME280_OVERSAMPLING_1X
-    if oversampling == BME280Oversampling.X2:
-        return BME280_OVERSAMPLING_2X
-    if oversampling == BME280Oversampling.X4:
-        return BME280_OVERSAMPLING_4X
-    if oversampling == BME280Oversampling.X8:
-        return BME280_OVERSAMPLING_8X
-    if oversampling == BME280Oversampling.X16:
-        return BME280_OVERSAMPLING_16X
-    return None
-
-cdef inline _map_standby_time(standby_time):
-    if standby_time == BME280StandbyTime.TIME_1_MS:
-        return BME280_STANDBY_TIME_1_MS
-    if standby_time == BME280StandbyTime.TIME_10_MS:
-        return BME280_STANDBY_TIME_10_MS
-    if standby_time == BME280StandbyTime.TIME_20_MS:
-        return BME280_STANDBY_TIME_20_MS
-    if standby_time == BME280StandbyTime.TIME_62_5_MS:
-        return BME280_STANDBY_TIME_62_5_MS
-    if standby_time == BME280StandbyTime.TIME_125_MS:
-        return BME280_STANDBY_TIME_125_MS
-    if standby_time == BME280StandbyTime.TIME_250_MS:
-        return BME280_STANDBY_TIME_250_MS
-    if standby_time == BME280StandbyTime.TIME_500_MS:
-        return BME280_STANDBY_TIME_500_MS
-    if standby_time == BME280StandbyTime.TIME_1000_MS:
-        return BME280_STANDBY_TIME_1000_MS
-    return None
-
-cdef inline _map_filter(filter_coeff):
-    if filter_coeff == BME280Filter.OFF:
-        return BME280_FILTER_COEFF_OFF
-    if filter_coeff == BME280Filter.COEFF_2:
-        return BME280_FILTER_COEFF_2
-    if filter_coeff == BME280Filter.COEFF_4:
-        return BME280_FILTER_COEFF_4
-    if filter_coeff == BME280Filter.COEFF_8:
-        return BME280_FILTER_COEFF_8
-    if filter_coeff == BME280Filter.COEFF_16:
-        return BME280_FILTER_COEFF_16
-    return None
+    OFF = 0
+    COEFF_2 = 1
+    COEFF_4 = 2
+    COEFF_8 = 3
+    COEFF_16 = 4
 
 
 cdef class BME280:
@@ -194,64 +137,46 @@ cdef class BME280:
         cdef int8_t rslt = BME280_OK
         cdef uint8_t settings_sel = 0
 
-        _sensor_mode = _map_mode(sensor_mode)
-        if _sensor_mode is None:
-            raise ValueError("sensor_mode needs to be of type BME280SensorPowerMode")
-
-        if _sensor_mode == BME280_NORMAL_MODE and standby_time is None:
+        if sensor_mode == BME280_NORMAL_MODE and standby_time is None:
             raise ValueError("standby_time must be set for NORMAL mode")
 
         sel_settings = []
         if humidity_oversampling is not None:
-            _humidity_oversampling = _map_oversampling(humidity_oversampling)
-            if _humidity_oversampling is None:
-                raise ValueError("humidity_oversampling needs to be of type BME280Oversampling")
-            self.dev.settings.osr_h = _humidity_oversampling
+            self.dev.settings.osr_h = humidity_oversampling
             sel_settings.append(BME280_OSR_HUM_SEL)
 
         if pressure_oversampling is not None:
-            _pressure_oversampling = _map_oversampling(pressure_oversampling)
-            if _pressure_oversampling is None:
-                raise ValueError("pressure_oversampling needs to be of type BME280Oversampling")
-            self.dev.settings.osr_p = _pressure_oversampling
+            self.dev.settings.osr_p = pressure_oversampling
             sel_settings.append(BME280_OSR_PRESS_SEL)
 
         if temperature_oversampling is not None:
-            _temperature_oversampling = _map_oversampling(temperature_oversampling)
-            if _temperature_oversampling is None:
-                raise ValueError("temperature_oversampling needs to be of type BME280Oversampling")
-            self.dev.settings.osr_t = _temperature_oversampling
+            self.dev.settings.osr_t = temperature_oversampling
             sel_settings.append(BME280_OSR_TEMP_SEL)
 
         if filter_coeff is not None:
-            _filter_coeff = _map_filter(filter_coeff)
-            if _filter_coeff is None:
-                raise ValueError("filter_coeff needs to be of type BME280Filter")
-            self.dev.settings.filter = _filter_coeff
+            self.dev.settings.filter = filter_coeff
             sel_settings.append(BME280_FILTER_SEL)
 
-        if _sensor_mode == BME280_NORMAL_MODE:
-            _standby_time = _map_standby_time(standby_time)
-            if _standby_time is None:
-                raise ValueError("standby_time needs to be of type BME280StandbyTime")
-            self.dev.settings.standby_time = _standby_time
+        if sensor_mode == BME280_NORMAL_MODE:
+            self.dev.settings.standby_time = standby_time
             sel_settings.append(BME280_STANDBY_SEL)
 
-        for _sel_setting in sel_settings:
-            settings_sel |= _sel_setting
+        for sel_setting in sel_settings:
+            settings_sel |= sel_setting
         rslt = bme280_set_sensor_settings(settings_sel,
                                           &self.dev)
         check_error(rslt, "bme280_set_sensor_settings")
 
-        rslt = bme280_set_sensor_mode(_sensor_mode,
+        rslt = bme280_set_sensor_mode(sensor_mode,
                                       &self.dev)
         check_error(rslt, "bme280_set_sensor_mode")
 
-    def poll(self):
+    def read(self):
         """
-        Returns a dictionary with sensor readings
+        Reads values from sensor.
 
-        The dictionary contains the following keys:
+        Returns a dictionary with sensor readings and
+        following keys:
         -'temperature', unit: degree Celsius
         - 'humidity', unit:  percentage relative humidity
         - 'pressure', unit: hectopascal
